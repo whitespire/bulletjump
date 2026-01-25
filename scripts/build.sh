3c08633c96b534a9dcfab2089d468c7483cf1640
Whitespire <whitespire@proton.me>
2026-01-25 09:44:57 +0200

    chore: license, migrate to hypixel's maven

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 mvn jar:jar
-cp ./target/BulletJump-0.2.0-SNAPSHOT.jar ~/.var/app/com.hypixel.HytaleLauncher/data/Hytale/UserData/Mods/
+cp ./target/BulletJump-0.3.0-SNAPSHOT.jar ~/.var/app/com.hypixel.HytaleLauncher/data/Hytale/UserData/Mods/
--- scripts/init.sh
+++ scripts/init.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 
- mvn install:install-file -Dfile=$HOME/.var/app/com.hypixel.HytaleLauncher/data/Hytale/install/release/package/game/latest/Server/HytaleServer.jar -DgroupId=com.hypixel.hytale -DartifactId=HytaleServer-parent -Dversion=1.0-SNAPSHOT -Dpackaging=jar
  mvn clean package
